2ad83a74e31e306546127759359177ab19f35748
Nicholas G Cotton <nic@nintu.net>
2023-06-12 14:57:37 -0400

    Update SendToWhisper-French.bat
    Updated with offline model_path instructions.

--- SendToWhisper-French.bat
+++ SendToWhisper-French.bat
@@ -3,9 +3,8 @@ set model=large
 set lang=French
 set task=translate
 REM Change lines 2-4 to make a new version of the batch file for another language. Don't forget to rename the new file to Whisper-LANGUAGE.bat
-REM You should NOT change anything below this point.
 REM Updated 2022-12-01 to add loop for mulitple files of the same language, so items can be queued to run one after another. 
-REM For offline models you must add --model_dir to the Whisper command. E.g. "--model_dir "C:\Users\Nicholas\.cache\whisper"
+REM Updated 2023-06-12 For offline models you must add --model_dir to the Whisper command. E.g. "--model_dir "C:\Users\Nicholas\.cache\whisper" at line 23.
 IF "%~1" == "" (GOTO NoSource)
 :loop
 IF "%~1" == "" (GOTO :Complete)

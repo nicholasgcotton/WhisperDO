24bf2cb54b9ec62c66a2f841b17d821889b5470b
Nicholas G Cotton, CIPT <nic@nintu.net>
2024-05-22 12:14:20 -0400

    Delete WhisperDO.py
    Use https://github.com/Purfview/whisper-standalone-win instead

--- WhisperDO.py
+++
@@ -1,4 +0,0 @@
-# A short python script to use the python version of Whisper instead of the CLI version, which has issues with non-English audio. 
-import whisper
-from whisper.transcribe import cli
-cli()
